724b7d0040a0c04cea4ab4aa4fdd40325ebb59d5
MMOURGUES <maxime.mourgues@ynov.com>
2023-04-27 16:52:25 +0200

    user

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAokeg8R9iYyTnfwgOKZ0RPb_Cm_R1tmgQ",
-  authDomain: "tres-lot.firebaseapp.com",
-  projectId: "tres-lot",
-  storageBucket: "tres-lot.appspot.com",
-  messagingSenderId: "138201779895",
-  appId: "1:138201779895:web:b98eb53716cc232de38732",
-  measurementId: "G-26JEJT8XJR"
+	apiKey: "AIzaSyAokeg8R9iYyTnfwgOKZ0RPb_Cm_R1tmgQ",
+	authDomain: "tres-lot.firebaseapp.com",
+	projectId: "tres-lot",
+	storageBucket: "tres-lot.appspot.com",
+	messagingSenderId: "138201779895",
+	appId: "1:138201779895:web:b98eb53716cc232de38732",
+	measurementId: "G-26JEJT8XJR"
 };
 
 // Initialize Firebase
