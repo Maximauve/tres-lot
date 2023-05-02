b7c5ef61755635dc9bd5f2b40324679234fb0686
MMOURGUES <maxime.mourgues@ynov.com>
2023-05-02 14:05:58 +0200

    indentation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,55 +1,55 @@
 import React, { useEffect } from 'react';
-import Navbar from 'src/components/navbar/Navbar';	
+import Navbar from 'src/components/navbar/Navbar';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { onAuthStateChanged } from "firebase/auth";
-import {  signOut } from "firebase/auth";
+import { signOut } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
 import { auth } from 'src/config/firebase';
- 
+
 const Home = () => {
-	
+
 	const [user, loading] = useAuthState(auth);
 
 	const navigate = useNavigate();
- 
-	const handleLogout = () => {               
+
+	const handleLogout = () => {
 		signOut(auth).then(() => {
-		// Sign-out successful.
+			// Sign-out successful.
 			navigate("/");
 			console.log("Signed out successfully")
 		}).catch((error) => {
 			console.log("Sign out error -> ", error)
 		});
 	}
- 
-	useEffect(()=>{
+
+	useEffect(() => {
 		onAuthStateChanged(auth, (user) => {
 			if (user) {
-			  const uid = user.uid;
-			  console.log("uid", uid)
+				const uid = user.uid;
+				console.log("uid", uid)
 			} else {
-			  console.log("user is logged out")
+				console.log("user is logged out")
 			}
-		  });
+		});
 		if (loading) return;
 		if (!user) return navigate("/login");
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [user, loading]);
- 
+
 	return (
 		<>
-				<Navbar />
-				<p>
-					Welcome Home
-				</p>
+			<Navbar />
+			<p>
+				Welcome Home
+			</p>
 
-				<div>
-					<button onClick={handleLogout}>
-						Logout
-					</button>
-				</div>
+			<div>
+				<button onClick={handleLogout}>
+					Logout
+				</button>
+			</div>
 		</>
 	)
 }
- 
+
 export default Home
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {  signInWithEmailAndPassword   } from 'firebase/auth';
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { NavLink, useNavigate } from 'react-router-dom'
 import "src/styles/Auth.scss";
 import Header from '../components/auth/Header';
@@ -7,51 +7,51 @@ import { auth } from 'src/config/firebase';
 import Input from 'src/components/auth/Input';
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(false);
+	const navigate = useNavigate();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState(false);
 
-    const onLogin = (e: any) => {
-        e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in
-            // const user = userCredential.user;
-            navigate("/home")
-        })
-        .catch(() => {
-            setError(true);
-        });
+	const onLogin = (e: any) => {
+		e.preventDefault();
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				// const user = userCredential.user;
+				navigate("/home")
+			})
+			.catch(() => {
+				setError(true);
+			});
 
-    }
+	}
 
-    return(
-        <>
-            <main>
-                <section>
-                    <div className="auth">
-                        <Header title="Connectez vous à votre compte"/>
-                        { error && <p className="auth-error">Mot de passe ou email incorrect</p> }
-                        <div className="auth-redirect">
-                            <p className="auth-text">Vous n'avez pas encore de compte ?</p>
-                            <NavLink to="/signup">
-                                Créer votre compte
-                            </NavLink>
-                        </div>
-                        <form>
-                            <Input id="email-address" name="email" type="email" placeholder="Adresse mail" onChange={(e) => setEmail(e.target.value)} />
-                            <Input id="password" name="password" type="password" placeholder="Mot de passe" onChange={(e) => setPassword(e.target.value)} />
-                            <button className="auth-button"
-                                onClick={onLogin}>
-                                Connexion
-                            </button>
-                        </form>
-                    </div>
-                </section>
-            </main>
-        </>
-    )
+	return (
+		<>
+			<main>
+				<section>
+					<div className="auth">
+						<Header title="Connectez vous à votre compte" />
+						{error && <p className="auth-error">Mot de passe ou email incorrect</p>}
+						<div className="auth-redirect">
+							<p className="auth-text">Vous n'avez pas encore de compte ?</p>
+							<NavLink to="/signup">
+								Créer votre compte
+							</NavLink>
+						</div>
+						<form>
+							<Input id="email-address" name="email" type="email" placeholder="Adresse mail" onChange={(e) => setEmail(e.target.value)} />
+							<Input id="password" name="password" type="password" placeholder="Mot de passe" onChange={(e) => setPassword(e.target.value)} />
+							<button className="auth-button"
+								onClick={onLogin}>
+								Connexion
+							</button>
+						</form>
+					</div>
+				</section>
+			</main>
+		</>
+	)
 }
 
 export default Login
