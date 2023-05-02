ba1c36962260615104428d239f4ddcd54c5eb78d
MMOURGUES <maxime.mourgues@ynov.com>
2023-05-02 22:32:37 +0200

    better modal display

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -3,26 +3,29 @@ import React, { PropsWithChildren } from 'react';
 import "src/styles/Modal.scss";
 
 interface Props {
+	title: string;
 	show: boolean;
 	setShow: (value: boolean) => void;
 }
 
 const Modal: React.FC<PropsWithChildren<Props>> = (props) => {
-	const { children, show, setShow } = props;
+	const { title, children, show, setShow } = props;
 
 	if (!show) {
 		return null;
 	}
 
 	return (
-		<div className="modal" onClick={() => setShow(false)}>
-			<div className="modal-block" onClick={e => e.stopPropagation()}>
+		<div className="modal" onMouseDown={() => setShow(false)}>
+			<div className="modal-block" onMouseDown={e => e.stopPropagation()}>
 				<button className="modal-close" onClick={() => setShow(false)}>
 					X
 				</button>
+				<div className='modal-title'>
+					<p>{title}</p>
+				</div>
 				{children}
 			</div>
-
 		</div>
 	);
 };
